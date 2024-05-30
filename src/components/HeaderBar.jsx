8033b90a2d44900d51fe1c1d2bdd75f6c9b7ae85
amanfangeria980 <amanfangeria980@gmail.com>
2024-05-30 15:35:30 +0530

    button color changedf

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -14,20 +14,21 @@ const HeaderBar = ({ options }) => {
 
   return (
     <div>
-      <div className="flex items-center m-8">
+      <div className="flex items-center m-8 flex-row">
         <h2 className="text-xl font-semibold mr-5">Trending</h2>
         <div className="relative w-56 p-1 bg-white rounded-full border border-black">
           <div
-            className={`absolute top-0 w-1/2 h-full bg-orange-500 rounded-full transition-transform duration-500 ${
+            className={`absolute top-0 w-1/2 h-full bg-[#032541] rounded-full transition-transform duration-500 ${
               selectedOption === "today"
                 ? "transform translate-x-0"
                 : "transform translate-x-full"
             }`}
           ></div>
+          <div></div>
           {options.map((option) => (
             <button
               key={option.value}
-              className={`relative z-10 w-1/2 p-2 text-center bg-transparent outline-none focus:outline-none ${
+              className={`relative z-10 w-1/2 p-2 text-center bg-transparent outline-none focus:outline-none text-[#389788] ${
                 selectedOption === option.value ? "text-white" : ""
               }`}
               onClick={() => handleOptionClick(option.value)}
