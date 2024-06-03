8358b99ef8533594da72b2c0a4770ad025a35721
amanfangeria980 <amanfangeria980@gmail.com>
2024-06-03 15:02:16 +0530

    ...

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import { formatDate, formatRuntime } from "./../../utils";
 import { IoOptionsOutline } from "react-icons/io5";
